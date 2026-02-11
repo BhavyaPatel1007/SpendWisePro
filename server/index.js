bab2a036da3d5281887b490c2a9071ef0b877650
Bhavya <bhavyapatel4141@gmail.com>
2026-02-11 17:16:36 +0530

    Fixed API URL

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    // origin: process.env.CLIENT_URL || "http://localhost:5173",
+    // credentials: true
+}));
 app.use(express.json());
 
 // Basic Route
