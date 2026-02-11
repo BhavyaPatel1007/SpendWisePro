67d624b0cd20f5edaa67df65447dff590786ab39
Bhavya <bhavyapatel4141@gmail.com>
2026-02-11 17:26:47 +0530

    Updated CORS for Vercel

--- server/index.js
+++ server/index.js
@@ -6,12 +6,18 @@ const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// app.use(cors({
+//     // origin: process.env.CLIENT_URL || "http://localhost:5173",
+//     // credentials: true,
+//     // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     // allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"]
+// }));
+
 app.use(cors({
-    // origin: process.env.CLIENT_URL || "http://localhost:5173",
-    // credentials: true,
-    // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    // allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"]
+    origin: "https://spend-wise-pro.vercel.app",
+    credentials: true
 }));
+
 app.use(express.json());
 
 // Basic Route
