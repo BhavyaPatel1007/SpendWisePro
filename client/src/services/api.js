2f521fbba507f56a5dc17f69c8d706bae0cfc3ea
Bhavya <bhavyapatel4141@gmail.com>
2026-02-11 17:19:55 +0530

    Fixed API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://spendwisepro.onrender.com/api'
+    baseURL: (import.meta.env.VITE_API_URL || 'http://localhost:5000') + '/api'
 });
 
 // Add tokens to every request
