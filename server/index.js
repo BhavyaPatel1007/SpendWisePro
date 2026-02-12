632fef9427455ddf9f2445b21a86dc154bb2d4e4
Bhavya <bhavyapatel4141@gmail.com>
2026-02-12 11:31:39 +0530

    Updated DB env variable names to MYSQL_*

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // }));
 
 app.use(cors({
-    origin: "https://spend-wise-pro.vercel.app",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
