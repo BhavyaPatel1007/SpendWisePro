be4e3d6a5d428a201a60cdd4af8208782e84c368
Bhavya <bhavyapatel4141@gmail.com>
2026-02-12 11:49:04 +0530

    Fixed CORS completely

--- server/index.js
+++ server/index.js
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 5000;
 // }));
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+        "https://spend-wise-pro-eight.vercel.app",
+        process.env.FRONTEND_URL,
+        "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
 
